613056454784fa4ce239ebfceaef175e88d1c536
LianQi-Kevin <18201584863@163.com>
2024-09-14 05:45:46 +0800

    * 移除codeCard component和shiki依赖项以减少打包体积

--- src/components/chatMessage.vue
+++ src/components/chatMessage.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import md from '@/markdown/markdown'
 import {CopyDocument, Help, Loading} from "@element-plus/icons-vue";
-import CodeCard from "@/components/codeCard.vue";
 import {useClipboard, useThrottleFn} from "@vueuse/core";
 
 import "@/styles/github-markdown.css"
@@ -13,40 +12,6 @@ export interface ChatMessagePropsType {
 }
 
 const props = defineProps<ChatMessagePropsType>()
-
-const throttledReplaceCodeCard = useThrottleFn(replaceCodeCard, 500);
-const codeBlocksContainer = ref<any>();
-
-onMounted(() => {
-  nextTick(() => {
-    throttledReplaceCodeCard()
-  });
-})
-
-function replaceCodeCard() {
-  // 获取 codeBlocksContainer 的实际 DOM 元素
-  const containerElement = codeBlocksContainer.value;
-
-  if (containerElement) {
-    const codeBlocks = containerElement.querySelectorAll('pre[id="codeSpace"]');
-
-    codeBlocks.forEach((block: Element) => {
-      const code = block.querySelector('code[class*="language-"]');
-      if (code) {
-        const content = code.textContent;
-        // 使用默认值 'plaintext' 如果没有找到语言类
-        const lang = code.className.replace('language-', '') || 'plaintext';
-
-        // 创建 CodeCard 实例并替换当前的 pre 元素
-        // 传递 lang, content, 以及 theme (如果需要的话)
-        const codeCardInstance = createApp(CodeCard, { lang, content });
-        const mountNode = document.createElement('div');
-        block.replaceWith(mountNode);
-        codeCardInstance.mount(mountNode);
-      }
-    });
-  }
-}
 
 const { copy, copied, isSupported } = useClipboard()
 const throttledCopy = useThrottleFn(copyContent, 500);
